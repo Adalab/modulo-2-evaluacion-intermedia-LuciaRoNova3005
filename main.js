a575289a0783dafc796cba7ad0f33b441f1fe5b9
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-12 10:31:36 +0200

    Add function

--- main.js
+++ main.js
@@ -2,15 +2,21 @@
 function getRandomNumber(max) {
   return Math.ceil(Math.random() * max);
 }
-
-const numberInput = document.querySelector(".js-number");
-const button = document.querySelector(".js-button");
-const help = document.querySelector(".js-help");
-const numberTry = document.querySelector("js-count");
-//const userNumber =
-
 const number = getRandomNumber(100);
+const numberInput = document.querySelector(".js-number");
+const numberInputValue = numberInput.value;
+const help = document.querySelector(".js-help");
+const button = document.querySelector(".js-button");
+const numberTry = document.querySelector(".js-count");
 
-console.log(number);
+function handelplay() {
+  if (numberInputValue > number) {
+    return (help.value = "Demasiado alto");
+  } else if (numberInputValue < number) {
+    return (help.value = "Demasiado bajo");
+  } else if (numberInputValue === number) {
+    return (help.value = "HAS GANADO");
+  }
+}
 
-//  button.addEventListener ("click", handelfunction)
+button.addEventListener("click", handelplay);
