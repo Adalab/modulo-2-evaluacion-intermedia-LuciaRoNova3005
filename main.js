da0660e0f3438567658ec68095085e48d6720682
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-12 09:24:19 +0200

    Add consts

--- main.js
+++ main.js
@@ -1,2 +1,18 @@
 "use script"
-console.log ("Hola")
+const numberInput = document.querySelector (".js-number");
+const button = document.querySelector (".js-button");
+const help = document.querySelector (".js-help");
+
+const numberTry = document.querySelector ("js-count")
+
+console.log (numberInput);
+
+   
+
+
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+   }
+   
+
+  
