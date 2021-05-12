578e1ae6f4cbf73ae4e345e75d1bbf70d2fb527b
LuciaRoNova3005 <lucia.r.nova@gmail.com>
2021-05-12 09:48:25 +0200

    Add const numberradon

--- main.js
+++ main.js
@@ -1,18 +1,16 @@
-"use script"
-const numberInput = document.querySelector (".js-number");
-const button = document.querySelector (".js-button");
-const help = document.querySelector (".js-help");
-
-const numberTry = document.querySelector ("js-count")
-
-console.log (numberInput);
-
-   
-
-
+"use script";
 function getRandomNumber(max) {
-    return Math.ceil(Math.random() * max);
-   }
-   
+  return Math.ceil(Math.random() * max);
+}
 
-  
+const numberInput = document.querySelector(".js-number");
+const button = document.querySelector(".js-button");
+const help = document.querySelector(".js-help");
+const numberTry = document.querySelector("js-count");
+//const userNumber =
+
+const number = getRandomNumber(100);
+
+console.log(number);
+
+//  button.addEventListener ("click", handelfunction)
